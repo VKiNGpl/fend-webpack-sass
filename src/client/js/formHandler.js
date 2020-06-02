1823d7e46315346f923fbe22780eb73a8ee3e5b7
Krystian Spiewak <krystianspiewak@gmail.com>
2020-06-01 22:12:54 -0500

    working weather API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,13 +3,16 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
+    let baseURL = 'api.openweathermap.org/data/2.5/weather?q='
+    let locationName = 'London,uk'
+    let apiKey = '4188e0776bf39eeccc86ef1d6cc901f5'
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(`http://${baseURL}${locationName}&APPID=${apiKey}`)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = res.weather[0].description
     })
 }
 
